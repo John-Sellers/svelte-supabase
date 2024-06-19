5720ee3d6d90687ce9e5fa1c4984d0b1fff3842a
John-Sellers <sellers.e.john@gmail.com>
2024-06-18 20:14:50 -0400

    Created specific user pages and and learned about reactive routing

---
+++ web-app/src/routes/[email]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import styles from "./Page.module.css";
+
+    $: email = $page.params.email;
+</script>
+
+<div class={styles.page}>
+    <div class={styles.heroContent}>
+        <div class={styles.heroText}>
+            <h1 class={styles.heroTitle}>{email} Page</h1>
+            <p>User description here...</p>
+        </div>
+    </div>
+</div>
